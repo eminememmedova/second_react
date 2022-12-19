9a295c2849155e29f4a016f58a12c444a4e24c0e
emine <memmedovaemine553@gmail.com>
2022-12-19 13:21:03 +0400

    second commit

--- second_react/src/App.js
+++ second_react/src/App.js
@@ -1,10 +1,19 @@
 
 import './App.css';
+import Navbar from './components/Navbar';
+import Footer from './components/Footer';
+import Test from './components/Test';
 
 function App() {
-  
+
   return (
-  <h1>Hello</h1>
+    <>
+    <h1>Hello</h1>
+    <Navbar/>
+    <Footer/>
+    <Test content = "Hello World"/>
+    </>
   );
 }
+
 export default App;
